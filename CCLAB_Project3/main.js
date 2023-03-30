6ae6221c9d3a1707fa933c7339e9c3a15103fe2f
chanc245 <112438908+chanc245@users.noreply.github.com>
2023-03-30 04:06:16 -0400

    roughly done
    without some details

---
+++ CCLAB_Project3/main.js
@@ -0,0 +1,10 @@
+var square = document.getElementById("square");
+
+document.addEventListener("mousemove", function (event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    square.style.left = x + "px";
+    square.style.top = y + "px";
+    square.textContent = "x: " + (x - 20) + ", y: " + (y - 20);
+    //I dont know why 20 conpensation is nesscory, but haha it works :)
+});
